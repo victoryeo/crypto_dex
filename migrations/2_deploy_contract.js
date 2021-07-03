a83df26dc838ae438cf10ad617e5e3642acd18e4
yeosiowvic <victoryeo1974@gmail.com>
2021-07-03 17:24:30 +0800

    update truffle deploy script

--- migrations/2_deploy_contract.js
+++ migrations/2_deploy_contract.js
@@ -2,11 +2,11 @@ var swap = artifacts.require("SimpleSwap");
 var token = artifacts.require("Token");
 
 module.exports = async function(deployer){
-  await deployer.deploy(token);
+  await deployer.deploy(token, "hello","world");
   const tokenInst = await token.deployed()
 
   await deployer.deploy(swap, tokenInst.address);
   const swapInst = await swap.deployed()
 
-  //await tokenInst.transfer(swapInst.address, '1000000000000000000000000')
+  await tokenInst.transfer(swapInst.address, '1000000000000000000000000')
 }
