b97c022f56106bbe626d919f71214f379e07614e
yeosiowvic <victoryeo1974@gmail.com>
2021-07-03 20:57:31 +0800

    add buy form

--- src/frontend/src/Main.js
+++ src/frontend/src/Main.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import BuyForm from './BuyForm'
 
 class Main extends Component {
     constructor(props) {
@@ -9,10 +10,29 @@ class Main extends Component {
     }
     render() {
         let content
+        if(this.state.currentForm === 'buy') {
+            content = <BuyForm
+              ethBalance={this.props.ethBalance}
+              tokenBalance={this.props.tokenBalance}
+              buyTokens={this.props.buyTokens}
+            />
+        }
         return (
-            <div className="card mb-4" >
-                <div className="card-body">
-                    {content}
+            <div id="content" className="mt-3">
+                <div className="d-flex justify-content-between mb-3">
+                <button
+                    className="btn btn-light"
+                    onClick={(event) => {
+                        this.setState({ currentForm: 'buy' })
+                    }}
+                    >
+                    Buy
+                </button>
+                </div>
+                <div className="card mb-4" >
+                    <div className="card-body">
+                        {content}
+                    </div>
                 </div>
             </div>
         )
