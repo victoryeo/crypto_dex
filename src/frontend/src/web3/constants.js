f32ab63eaec27096f8d8ef5f2d1f791548a8056d
yeosiowvic <victoryeo1974@gmail.com>
2021-07-06 17:25:16 +0800

    change ganache port to 9545

--- src/frontend/src/web3/constants.js
+++ src/frontend/src/web3/constants.js
@@ -3,7 +3,7 @@ export const hdPathString = `m/44'/60'/0'/0`
 /* keystore will be saved to local storage under this key */
 export const localStorageKey = 'ks'
 
-export const ganachehost = 'http://localhost:7545'
+export const ganachehost = 'http://localhost:9545'
 
 /*usage: convert amount to wei
 const sendAmount = new BigNumber(amount).times(Ether);
