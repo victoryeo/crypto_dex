58af4cf9494da521ad643ae0e16eb4a76583026e
yeosiowvic <victoryeo1974@gmail.com>
2021-07-08 14:16:27 +0800

    add buyTokens test case

--- test/SimpleSwap.test.js
+++ test/SimpleSwap.test.js
@@ -7,7 +7,10 @@ const { should } = require('chai');
 should()
 
 function tokens(n) {
-  return web3.utils.toWei(n, 'ether')
+  console.log(n)
+  let newvalue = web3.utils.toWei(n, 'ether')
+  console.log(newvalue)
+  return newvalue
 }
 
 contract('SimpleSwap', ([deployer, investor]) => {
@@ -32,5 +35,36 @@ contract('SimpleSwap', ([deployer, investor]) => {
           let balance = await token.balanceOf(simpleSwap.address)
           assert.equal(balance.toString(), tokens('1000000'))
         })
+    })
+
+    describe('buyTokens()', async () => {
+        let result
+    
+        before(async () => {
+          // Purchase tokens before each example
+          result = await simpleSwap.buyTokens({ from: investor, 
+            value: web3.utils.toWei('1', 'ether')})
+        })
+
+        it('Allows user to purchase tokens from simpleSwap for a fixed price', async () => {
+            // Check investor token balance after purchase
+            let investorBalance = await token.balanceOf(investor)
+            assert.equal(investorBalance.toString(), tokens('100'))
+      
+            // Check simpleSwap balance after purchase
+            let simpleSwapBalance
+            simpleSwapBalance = await token.balanceOf(simpleSwap.address)
+            assert.equal(simpleSwapBalance.toString(), tokens('999900'))
+            simpleSwapBalance = await web3.eth.getBalance(simpleSwap.address)
+            assert.equal(simpleSwapBalance.toString(), web3.utils.toWei('1', 'Ether'))
+      
+            // Check logs to ensure event was emitted with correct data
+            console.log(result.logs)
+            const event = result.logs[0].args
+            assert.equal(event.account, investor)
+            assert.equal(event.token, token.address)
+            assert.equal(event.amount.toString(), tokens('100').toString())
+            assert.equal(event.rate.toString(), '100')
+          })
     })
 })  
