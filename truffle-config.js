a316ccdcc0235aeb3135bf22104681e7e67bb14f
yeosiowvic <victoryeo1974@gmail.com>
2021-08-21 13:55:22 +0800

    add deployment to rinkeby

--- truffle-config.js
+++ truffle-config.js
@@ -1,3 +1,9 @@
+require("dotenv").config();
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+
+const { MNEMONIC, INFURA_API_KEY } = process.env;
+const rinkebyUrl = `https://rinkeby.infura.io/v3/${INFURA_API_KEY}`;
+
 module.exports = {
   // Uncommenting the defaults below 
   // provides for an easier quick-start with Ganache.
@@ -11,6 +17,12 @@ module.exports = {
       port: 9545,
       network_id: "*"
     },
+    rinkeby: {
+      provider: () => new HDWalletProvider(MNEMONIC, rinkebyUrl),
+      network_id: 4,
+      networkCheckTimeout: 999999,
+      gas: 4612388 // Gas limit used for deploys
+    }
   },
   contracts_directory: './src/contracts/',
   contracts_build_directory: './build/contracts/',
